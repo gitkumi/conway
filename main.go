3f20e566c3ccee41024f8f04d7b772909fa563e8
Takumi <ito.tkmi@gmail.com>
2023-08-26 02:54:44 +0900

    Create basic board

--- main.go
+++ main.go
@@ -1,30 +1,100 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 420
+	screenHeight = 420
+	boardSize    = 10
+	tileSize     = 42
+)
+
+var (
+	black = color.RGBA{R: 0, G: 0, B: 0, A: 255}
+	white = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+)
+
+type Tile struct {
+	on    bool
+	x     int
+	y     int
+	image *ebiten.Image
+}
+
+type Board struct {
+	size  int
+	tiles map[*Tile]struct{}
+	image *ebiten.Image
+}
+
+type Game struct {
+	board *Board
+}
+
+func createTile(x, y int) *Tile {
+	image := ebiten.NewImage(tileSize, tileSize)
+	image.Fill(black)
+
+	return &Tile{
+		on:    false,
+		image: image,
+		x:     x,
+		y:     y,
+	}
+}
+
+func createBoard(size int) *Board {
+	image := ebiten.NewImage(screenWidth, screenHeight)
+	tiles := make(map[*Tile]struct{})
+
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			tile := createTile(x, y)
+			tiles[tile] = struct{}{}
+		}
+	}
+
+	return &Board{
+		size:  size,
+		tiles: tiles,
+		image: image,
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 210, 210
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
+	screen.DrawImage(g.board.image, nil)
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	for tile := range g.board.tiles {
+		op := &ebiten.DrawImageOptions{}
+
+		op.GeoM.Translate(float64(tileSize*tile.x), float64(tileSize*tile.y))
+		screen.DrawImage(tile.image, op)
+	}
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Tiles")
+	board := createBoard(boardSize)
+
+	g := &Game{
+		board: board,
+	}
+
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
